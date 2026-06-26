d9ee1391717ea2514af46a0010adb36c02cae2ff
thartsockLyricCapital <thartsock@lyriccapitalgroup.com>
2026-06-26 18:23:58 -0400

    Add files via upload

---
+++ royalty.js
@@ -0,0 +1,830 @@
+// Songs of Spirit royalties — pulled live from Sigma/Snowflake (SPIRIT_NPS_REPORT_CURRENT, SOS catalogue).
+// Spirit NPS lifetime + trailing-12-mo (through May 2026) + lifetime income, USD. Built by royalty_build.py.
+window.LYRIC_ROYALTY = {
+ "meta": {
+  "period": "Through May 2026 statements",
+  "basis": "Spirit Net Publisher Share (NPS), USD",
+  "window": "TTM = Jun 2025-May 2026",
+  "catalogue": "Songs of Spirit (SOS)",
+  "source": "Sigma / Snowflake — SPIRIT_NPS_REPORT_CURRENT (live pull)",
+  "totals": {
+   "npsLife": 1904350,
+   "npsTtm": 1497550,
+   "incomeLife": 5048387,
+   "songs": 3388,
+   "clients": 135
+  }
+ },
+ "writers": {
+  "Jeremy Bussey": {
+   "name": "Jeremy Bussey",
+   "npsLife": 49584,
+   "npsTtm": 49248,
+   "incomeLife": 84094
+  },
+  "Jason Vance Harris": {
+   "name": "Jason Vance Harris",
+   "npsLife": 2133,
+   "npsTtm": 2071,
+   "incomeLife": 7564
+  },
+  "Jessica Cayne": {
+   "name": "Jessica Cayne",
+   "npsLife": 2024,
+   "npsTtm": 1885,
+   "incomeLife": 2179
+  },
+  "Nick Gale (NG Publishing)": {
+   "name": "Nick Gale (NG Publishing)",
+   "npsLife": 93452,
+   "npsTtm": 71511,
+   "incomeLife": 246640
+  },
+  "Monty Criswell": {
+   "name": "Monty Criswell",
+   "npsLife": 60986,
+   "npsTtm": 54680,
+   "incomeLife": 138283
+  },
+  "Cale Dodds": {
+   "name": "Cale Dodds",
+   "npsLife": 26300,
+   "npsTtm": 4007,
+   "incomeLife": 105300
+  },
+  "Justin Richards": {
+   "name": "Justin Richards",
+   "npsLife": 58019,
+   "npsTtm": 53629,
+   "incomeLife": 79655
+  },
+  "David Garcia": {
+   "name": "David Garcia",
+   "npsLife": 147591,
+   "npsTtm": 48519,
+   "incomeLife": 398017
+  },
+  "Noelle Scaggs": {
+   "name": "Noelle Scaggs",
+   "npsLife": 1696,
+   "npsTtm": 282,
+   "incomeLife": 17022
+  },
+  "Bobby Hamrick": {
+   "name": "Bobby Hamrick",
+   "npsLife": 29223,
+   "npsTtm": 27951,
+   "incomeLife": 76322
+  },
+  "Ely Rise": {
+   "name": "Ely Rise",
+   "npsLife": 10849,
+   "npsTtm": 10849,
+   "incomeLife": 41208
+  },
+  "Camylio": {
+   "name": "Camylio",
+   "npsLife": 16277,
+   "npsTtm": 16221,
+   "incomeLife": 55154
+  },
+  "LECADE": {
+   "name": "LECADE",
+   "npsLife": 1842,
+   "npsTtm": 1842,
+   "incomeLife": 7082
+  },
+  "Songs of Spirit (house)": {
+   "name": "Songs of Spirit (house)",
+   "npsLife": 7141,
+   "npsTtm": 6216,
+   "incomeLife": 7141
+  },
+  "JJ Scheff": {
+   "name": "JJ Scheff",
+   "npsLife": 5782,
+   "npsTtm": 5782,
+   "incomeLife": 23578
+  },
+  "Autumn Rowe": {
+   "name": "Autumn Rowe",
+   "npsLife": 5171,
+   "npsTtm": 3401,
+   "incomeLife": 30898
+  },
+  "Mira Housey": {
+   "name": "Mira Housey",
+   "npsLife": 4815,
+   "npsTtm": 4722,
+   "incomeLife": 15815
+  },
+  "Melodie Wagner": {
+   "name": "Melodie Wagner",
+   "npsLife": 4115,
+   "npsTtm": 3518,
+   "incomeLife": 13301
+  },
+  "Rich Robinson": {
+   "name": "Rich Robinson",
+   "npsLife": 15398,
+   "npsTtm": 8062,
+   "incomeLife": 100592
+  },
+  "Ryan Beaver": {
+   "name": "Ryan Beaver",
+   "npsLife": 23470,
+   "npsTtm": 18496,
+   "incomeLife": 83868
+  },
+  "Patrick Ridgen": {
+   "name": "Patrick Ridgen",
+   "npsLife": 17272,
+   "npsTtm": 4367,
+   "incomeLife": 35236
+  },
+  "Rap-A-Lot (N-The-Water)": {
+   "name": "Rap-A-Lot (N-The-Water)",
+   "npsLife": 137720,
+   "npsTtm": 106838,
+   "incomeLife": 1021973
+  },
+  "Justin Halpin": {
+   "name": "Justin Halpin",
+   "npsLife": 23555,
+   "npsTtm": 10339,
+   "incomeLife": 79485
+  },
+  "Chizzy": {
+   "name": "Chizzy",
+   "npsLife": 8870,
+   "npsTtm": 5254,
+   "incomeLife": 50037
+  },
+  "Shane Stevens": {
+   "name": "Shane Stevens",
+   "npsLife": 3928,
+   "npsTtm": 2225,
+   "incomeLife": 15287
+  },
+  "Joy Williams": {
+   "name": "Joy Williams",
+   "npsLife": 2940,
+   "npsTtm": 1556,
+   "incomeLife": 11613
+  },
+  "Michael Hardy": {
+   "name": "Michael Hardy",
+   "npsLife": 855906,
+   "npsTtm": 855906,
+   "incomeLife": 1336322
+  },
+  "Sawyer Utah": {
+   "name": "Sawyer Utah",
+   "npsLife": 1595,
+   "npsTtm": 997,
+   "incomeLife": 3395
+  },
+  "Christopher M. Robinson": {
+   "name": "Christopher M. Robinson",
+   "npsLife": 12907,
+   "npsTtm": 6877,
+   "incomeLife": 84393
+  },
+  "Brent Anderson": {
+   "name": "Brent Anderson",
+   "npsLife": 143045,
+   "npsTtm": 46741,
+   "incomeLife": 310311
+  },
+  "Adam Wood": {
+   "name": "Adam Wood",
+   "npsLife": 2567,
+   "npsTtm": 2232,
+   "incomeLife": 6528
+  },
+  "Derek George": {
+   "name": "Derek George",
+   "npsLife": 39872,
+   "npsTtm": 37739,
+   "incomeLife": 92676
+  },
+  "Remy Gautreau": {
+   "name": "Remy Gautreau",
+   "npsLife": 38848,
+   "npsTtm": 5744,
+   "incomeLife": 134991
+  },
+  "Michael Fiorentino": {
+   "name": "Michael Fiorentino",
+   "npsLife": 25232,
+   "npsTtm": 772,
+   "incomeLife": 52721
+  }
+ },
+ "songs": {
+  "sand in my boots": {
+   "title": "Sand In My Boots",
+   "npsLife": 78227,
+   "npsTtm": 78227,
+   "incomeLife": 78227
+  },
+  "god's country": {
+   "title": "God'S Country",
+   "npsLife": 78159,
+   "npsTtm": 78159,
+   "incomeLife": 78159
+  },
+  "more than my hometown": {
+   "title": "More Than My Hometown",
+   "npsLife": 58295,
+   "npsTtm": 58295,
+   "incomeLife": 58295
+  },
+  "still waters": {
+   "title": "Still Waters",
+   "npsLife": 56738,
+   "npsTtm": 52446,
+   "incomeLife": 77355
+  },
+  "i got better": {
+   "title": "I Got Better",
+   "npsLife": 7521,
+   "npsTtm": 7521,
+   "incomeLife": 250701
+  },
+  "count me out": {
+   "title": "Count Me Out",
+   "npsLife": 7098,
+   "npsTtm": 7098,
+   "incomeLife": 26344
+  },
+  "dollars and sense": {
+   "title": "Dollars And Sense",
+   "npsLife": 3842,
+   "npsTtm": 3842,
+   "incomeLife": 28459
+  },
+  "jim bob": {
+   "title": "Jim Bob",
+   "npsLife": 7441,
+   "npsTtm": 5233,
+   "incomeLife": 26135
+  },
+  "whatcha know 'bout that": {
+   "title": "Whatcha Know 'Bout That",
+   "npsLife": 3768,
+   "npsTtm": 3768,
+   "incomeLife": 3768
+  },
+  "baby": {
+   "title": "Baby",
+   "npsLife": 12709,
+   "npsTtm": 9644,
+   "incomeLife": 94172
+  },
+  "livin' the dream": {
+   "title": "Livin' The Dream",
+   "npsLife": 11802,
+   "npsTtm": 11802,
+   "incomeLife": 11802
+  },
+  "if i know me": {
+   "title": "If I Know Me",
+   "npsLife": 11580,
+   "npsTtm": 11580,
+   "incomeLife": 11580
+  },
+  "fall of summer": {
+   "title": "Fall Of Summer",
+   "npsLife": 35510,
+   "npsTtm": 28737,
+   "incomeLife": 73573
+  },
+  "bottle rockets": {
+   "title": "Bottle Rockets",
+   "npsLife": 155344,
+   "npsTtm": 155344,
+   "incomeLife": 318293
+  },
+  "simple": {
+   "title": "Simple",
+   "npsLife": 23788,
+   "npsTtm": 23788,
+   "incomeLife": 23788
+  },
+  "still goin down": {
+   "title": "Still Goin Down",
+   "npsLife": 16800,
+   "npsTtm": 16800,
+   "incomeLife": 16800
+  },
+  "show up": {
+   "title": "Show Up",
+   "npsLife": 16012,
+   "npsTtm": 3079,
+   "incomeLife": 32210
+  },
+  "come back as a country boy": {
+   "title": "Come Back As A Country Boy",
+   "npsLife": 15879,
+   "npsTtm": 15879,
+   "incomeLife": 15879
+  },
+  "live for days": {
+   "title": "Live For Days",
+   "npsLife": 25062,
+   "npsTtm": 42,
+   "incomeLife": 83420
+  },
+  "tn fan": {
+   "title": "Tn Fan",
+   "npsLife": 15607,
+   "npsTtm": 15607,
+   "incomeLife": 15607
+  },
+  "livin' that good life": {
+   "title": "Livin' That Good Life",
+   "npsLife": 3695,
+   "npsTtm": 33,
+   "incomeLife": 13345
+  },
+  "this bar": {
+   "title": "This Bar",
+   "npsLife": 22689,
+   "npsTtm": 22689,
+   "incomeLife": 22689
+  },
+  "make you look": {
+   "title": "Make You Look",
+   "npsLife": 19576,
+   "npsTtm": 0,
+   "incomeLife": 67503
+  },
+  "drinkaby": {
+   "title": "Drinkaby",
+   "npsLife": 19424,
+   "npsTtm": 19424,
+   "incomeLife": 19424
+  },
+  "hate my heart": {
+   "title": "Hate My Heart",
+   "npsLife": 5252,
+   "npsTtm": 5252,
+   "incomeLife": 5252
+  },
+  "better boy": {
+   "title": "Better Boy",
+   "npsLife": 5098,
+   "npsTtm": 5098,
+   "incomeLife": 5098
+  },
+  "see you next summer": {
+   "title": "See You Next Summer",
+   "npsLife": 4916,
+   "npsTtm": 4916,
+   "incomeLife": 4916
+  },
+  "mine tomorrow": {
+   "title": "Mine Tomorrow",
+   "npsLife": 4225,
+   "npsTtm": 4225,
+   "incomeLife": 12985
+  },
+  "fuck faces": {
+   "title": "Fuck Faces",
+   "npsLife": 4042,
+   "npsTtm": 3006,
+   "incomeLife": 29959
+  },
+  "somethin' country": {
+   "title": "Somethin' Country",
+   "npsLife": 3939,
+   "npsTtm": 3939,
+   "incomeLife": 3939
+  },
+  "born with a beer in my hand": {
+   "title": "Born With A Beer In My Hand",
+   "npsLife": 14597,
+   "npsTtm": 14597,
+   "incomeLife": 14597
+  },
+  "beers on me": {
+   "title": "Beers On Me",
+   "npsLife": 14250,
+   "npsTtm": 14250,
+   "incomeLife": 14250
+  },
+  "new truck": {
+   "title": "New Truck",
+   "npsLife": 14010,
+   "npsTtm": 14010,
+   "incomeLife": 14010
+  },
+  "i don't know about you": {
+   "title": "I Don'T Know About You",
+   "npsLife": 12917,
+   "npsTtm": 12917,
+   "incomeLife": 12917
+  },
+  "if you were mine": {
+   "title": "If You Were Mine",
+   "npsLife": 12743,
+   "npsTtm": 12743,
+   "incomeLife": 40953
+  },
+  "back then right now": {
+   "title": "Back Then Right Now",
+   "npsLife": 108471,
+   "npsTtm": 16079,
+   "incomeLife": 263253
+  },
+  "wait in the truck": {
+   "title": "Wait In The Truck",
+   "npsLife": 103555,
+   "npsTtm": 103555,
+   "incomeLife": 103555
+  },
+  "cab in a solo": {
+   "title": "Cab In A Solo",
+   "npsLife": 101342,
+   "npsTtm": 9350,
+   "incomeLife": 211640
+  },
+  "rolex on a redneck": {
+   "title": "Rolex On A Redneck",
+   "npsLife": 7663,
+   "npsTtm": 7663,
+   "incomeLife": 7663
+  },
+  "nasty": {
+   "title": "Nasty",
+   "npsLife": 7618,
+   "npsTtm": 6037,
+   "incomeLife": 56438
+  },
+  "some girls": {
+   "title": "Some Girls",
+   "npsLife": 7590,
+   "npsTtm": 7590,
+   "incomeLife": 7590
+  },
+  "beer don't": {
+   "title": "Beer Don'T",
+   "npsLife": 4824,
+   "npsTtm": 4824,
+   "incomeLife": 4824
+  },
+  "waiting my whole life for this": {
+   "title": "Waiting My Whole Life For This",
+   "npsLife": 4788,
+   "npsTtm": 210,
+   "incomeLife": 16498
+  },
+  "do u?": {
+   "title": "Do U?",
+   "npsLife": 4737,
+   "npsTtm": 3521,
+   "incomeLife": 35102
+  },
+  "high, high, high": {
+   "title": "High, High, High",
+   "npsLife": 4645,
+   "npsTtm": 4645,
+   "incomeLife": 15208
+  },
+  "one big country song": {
+   "title": "One Big Country Song",
+   "npsLife": 4481,
+   "npsTtm": 4481,
+   "incomeLife": 4481
+  },
+  "single saturday night": {
+   "title": "Single Saturday Night",
+   "npsLife": 50622,
+   "npsTtm": 50622,
+   "incomeLife": 50622
+  },
+  "i wrote the book": {
+   "title": "I Wrote The Book",
+   "npsLife": 46797,
+   "npsTtm": 46797,
+   "incomeLife": 46797
+  },
+  "wanting and waiting": {
+   "title": "Wanting And Waiting",
+   "npsLife": 8105,
+   "npsTtm": 4319,
+   "incomeLife": 51923
+  },
+  "devil you know": {
+   "title": "Devil You Know",
+   "npsLife": 8066,
+   "npsTtm": 499,
+   "incomeLife": 9100
+  },
+  "still": {
+   "title": "Still",
+   "npsLife": 7979,
+   "npsTtm": 7781,
+   "incomeLife": 59074
+  },
+  "mind playin tricks": {
+   "title": "Mind Playin Tricks",
+   "npsLife": 8220,
+   "npsTtm": 6138,
+   "incomeLife": 60908
+  },
+  "good on you": {
+   "title": "Good On You",
+   "npsLife": 3434,
+   "npsTtm": 1126,
+   "incomeLife": 11246
+  },
+  "can a heart take": {
+   "title": "Can A Heart Take",
+   "npsLife": 3409,
+   "npsTtm": 3071,
+   "incomeLife": 10477
+  },
+  "practice": {
+   "title": "Practice",
+   "npsLife": 5910,
+   "npsTtm": 7,
+   "incomeLife": 23635
+  },
+  "up down": {
+   "title": "Up Down",
+   "npsLife": 34213,
+   "npsTtm": 34213,
+   "incomeLife": 34213
+  },
+  "foolish": {
+   "title": "Foolish",
+   "npsLife": 11496,
+   "npsTtm": 8779,
+   "incomeLife": 84547
+  },
+  "always on time": {
+   "title": "Always On Time",
+   "npsLife": 10239,
+   "npsTtm": 7960,
+   "incomeLife": 75877
+  },
+  "pray your name": {
+   "title": "Pray Your Name",
+   "npsLife": 3922,
+   "npsTtm": 2878,
+   "incomeLife": 12018
+  },
+  "pass the bar": {
+   "title": "Pass The Bar",
+   "npsLife": 3869,
+   "npsTtm": 855,
+   "incomeLife": 9414
+  },
+  "talk you out of it": {
+   "title": "Talk You Out Of It",
+   "npsLife": 6332,
+   "npsTtm": 6332,
+   "incomeLife": 6332
+  },
+  "holy water": {
+   "title": "Holy Water",
+   "npsLife": 6153,
+   "npsTtm": 6153,
+   "incomeLife": 6153
+  },
+  "church": {
+   "title": "Church",
+   "npsLife": 6089,
+   "npsTtm": 6089,
+   "incomeLife": 6089
+  },
+  "damn it feels good to be a gangsta": {
+   "title": "Damn It Feels Good To Be A Gangsta",
+   "npsLife": 6564,
+   "npsTtm": 4908,
+   "incomeLife": 48639
+  },
+  "po pimp": {
+   "title": "Po Pimp",
+   "npsLife": 6475,
+   "npsTtm": 4647,
+   "incomeLife": 47988
+  },
+  "confetti": {
+   "title": "Confetti",
+   "npsLife": 6333,
+   "npsTtm": 0,
+   "incomeLife": 25331
+  },
+  "redneck love song": {
+   "title": "Redneck Love Song",
+   "npsLife": 3384,
+   "npsTtm": 3384,
+   "incomeLife": 3384
+  },
+  "killing me": {
+   "title": "Killing Me",
+   "npsLife": 3352,
+   "npsTtm": 3352,
+   "incomeLife": 9295
+  },
+  "take her home": {
+   "title": "Take Her Home",
+   "npsLife": 44979,
+   "npsTtm": 44979,
+   "incomeLife": 44979
+  },
+  "one beer": {
+   "title": "One Beer",
+   "npsLife": 38074,
+   "npsTtm": 38074,
+   "incomeLife": 38074
+  }
+ },
+ "topWriters": [
+  {
+   "name": "Michael Hardy",
+   "npsLife": 855906,
+   "npsTtm": 855906,
+   "incomeLife": 1336322
+  },
+  {
+   "name": "David Garcia",
+   "npsLife": 147591,
+   "npsTtm": 48519,
+   "incomeLife": 398017
+  },
+  {
+   "name": "Brent Anderson",
+   "npsLife": 143045,
+   "npsTtm": 46741,
+   "incomeLife": 310311
+  },
+  {
+   "name": "Rap-A-Lot (N-The-Water)",
+   "npsLife": 137720,
+   "npsTtm": 106838,
+   "incomeLife": 1021973
+  },
+  {
+   "name": "Nick Gale (NG Publishing)",
+   "npsLife": 93452,
+   "npsTtm": 71511,
+   "incomeLife": 246640
+  },
+  {
+   "name": "Monty Criswell",
+   "npsLife": 60986,
+   "npsTtm": 54680,
+   "incomeLife": 138283
+  },
+  {
+   "name": "Justin Richards",
+   "npsLife": 58019,
+   "npsTtm": 53629,
+   "incomeLife": 79655
+  },
+  {
+   "name": "Jeremy Bussey",
+   "npsLife": 49584,
+   "npsTtm": 49248,
+   "incomeLife": 84094
+  },
+  {
+   "name": "Derek George",
+   "npsLife": 39872,
+   "npsTtm": 37739,
+   "incomeLife": 92676
+  },
+  {
+   "name": "Remy Gautreau",
+   "npsLife": 38848,
+   "npsTtm": 5744,
+   "incomeLife": 134991
+  },
+  {
+   "name": "Bobby Hamrick",
+   "npsLife": 29223,
+   "npsTtm": 27951,
+   "incomeLife": 76322
+  },
+  {
+   "name": "Cale Dodds",
+   "npsLife": 26300,
+   "npsTtm": 4007,
+   "incomeLife": 105300
+  },
+  {
+   "name": "Michael Fiorentino",
+   "npsLife": 25232,
+   "npsTtm": 772,
+   "incomeLife": 52721
+  },
+  {
+   "name": "Justin Halpin",
+   "npsLife": 23555,
+   "npsTtm": 10339,
+   "incomeLife": 79485
+  },
+  {
+   "name": "Ryan Beaver",
+   "npsLife": 23470,
+   "npsTtm": 18496,
+   "incomeLife": 83868
+  }
+ ],
+ "topSongs": [
+  {
+   "title": "Bottle Rockets",
+   "npsLife": 155344,
+   "npsTtm": 155344,
+   "incomeLife": 318293
+  },
+  {
+   "title": "Back Then Right Now",
+   "npsLife": 108471,
+   "npsTtm": 16079,
+   "incomeLife": 263253
+  },
+  {
+   "title": "Wait In The Truck",
+   "npsLife": 103555,
+   "npsTtm": 103555,
+   "incomeLife": 103555
+  },
+  {
+   "title": "Cab In A Solo",
+   "npsLife": 101342,
+   "npsTtm": 9350,
+   "incomeLife": 211640
+  },
+  {
+   "title": "Sand In My Boots",
+   "npsLife": 78227,
+   "npsTtm": 78227,
+   "incomeLife": 78227
+  },
+  {
+   "title": "God'S Country",
+   "npsLife": 78159,
+   "npsTtm": 78159,
+   "incomeLife": 78159
+  },
+  {
+   "title": "More Than My Hometown",
+   "npsLife": 58295,
+   "npsTtm": 58295,
+   "incomeLife": 58295
+  },
+  {
+   "title": "Still Waters",
+   "npsLife": 56738,
+   "npsTtm": 52446,
+   "incomeLife": 77355
+  },
+  {
+   "title": "Single Saturday Night",
+   "npsLife": 50622,
+   "npsTtm": 50622,
+   "incomeLife": 50622
+  },
+  {
+   "title": "I Wrote The Book",
+   "npsLife": 46797,
+   "npsTtm": 46797,
+   "incomeLife": 46797
+  },
+  {
+   "title": "Take Her Home",
+   "npsLife": 44979,
+   "npsTtm": 44979,
+   "incomeLife": 44979
+  },
+  {
+   "title": "One Beer",
+   "npsLife": 38074,
+   "npsTtm": 38074,
+   "incomeLife": 38074
+  },
+  {
+   "title": "Fall Of Summer",
+   "npsLife": 35510,
+   "npsTtm": 28737,
+   "incomeLife": 73573
+  },
+  {
+   "title": "Up Down",
+   "npsLife": 34213,
+   "npsTtm": 34213,
+   "incomeLife": 34213
+  },
+  {
+   "title": "Live For Days",
+   "npsLife": 25062,
+   "npsTtm": 42,
+   "incomeLife": 83420
+  }
+ ]
+};
